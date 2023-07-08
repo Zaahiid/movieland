38e0c0ca0192e41be64f80b9bb1185f2c3060521
asifSoftw <asif.softw@gmail.com>
2023-07-08 16:59:52 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
 
   return (
     <div className="app">
-      <h1>New MovieLand</h1>
+      <h1>New New MovieLand</h1>
       <div className="search">
         <input
           placeholder="Search for movies"
